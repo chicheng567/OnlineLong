aad90c5acb5dc1c08e482af225b51dd8acb97c1a
chicheng567 <chicheng567@gmail.com>
2026-03-10 11:28:41 +0000

    add checking health script

---
+++ dataset_util/check_video_health.py
@@ -0,0 +1,22 @@
+from videollama3.mm_utils import load_video
+from glob import glob
+import argparse
+from tqdm import tqdm
+parser = argparse.ArgumentParser()
+parser.add_argument("--root_path", type=str, help="Root path containing videos")
+parser.add_argument("--output_log_path", type=str, help="Path to output log file")
+args = parser.parse_args()
+
+if __name__ == "__main__":
+    root_path = args.root_path
+    output_log_path = args.output_log_path
+    all_video_paths = glob(root_path + "/*")
+    deprecated_count = 0
+    for video_path in tqdm(all_video_paths, desc="Checking video health"):
+        if video_path.endswith(".mp4") or video_path.endswith(".avi") or video_path.endswith(".mkv") or video_path.endswith(".webm") or video_path.endswith(".mov"):
+            frames, timestamps = load_video(video_path, fps=1, max_frames=200)
+            if len(frames) == 0 or frames is None or timestamps is None:
+                with open(output_log_path, "a") as f:
+                    f.write(video_path + "\n")
+                deprecated_count += 1
+    print(f"Total deprecated videos: {deprecated_count}")
